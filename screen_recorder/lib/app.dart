692659557984c027c9667d29087b351c0288df10
mesteranas <135442710+mesteranas@users.noreply.github.com>
2024-07-04 14:30:47 +0300

    04/07/2024

---
+++ screen_recorder/lib/app.dart
@@ -0,0 +1,7 @@
+class App{
+  static const name="screen recorder";
+  static const appName="screen_recorder_flutter";
+  static const appDirName="anasmain";
+  static const version=1.0;
+  static const description="";
+}
---
+++ screen_recorder/lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:screen_recorder/screen_recorder.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'settings.dart';
+import 'package:flutter/widgets.dart';
+
+import 'language.dart';
+import 'package:http/http.dart' as http;
+import 'viewText.dart';
+import 'app.dart';
+import 'contectUs.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() async{
+  await WidgetsFlutterBinding.ensureInitialized();
+  await Language.runTranslation();
+  runApp(test());
+}
+class test extends StatefulWidget{
+  const test({Key?key}):super(key:key);
+  @override
+  State<test> createState()=>_test();
+}
+class _test extends State<test>{
+  var isRecording=false;
+  var _=Language.translate;
+  ScreenRecorderController controller = ScreenRecorderController();
+  _test();
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      locale: Locale(Language.languageCode),
+      title: App.name,
+      themeMode: ThemeMode.system,
+      home:Builder(builder:(context) 
+    =>Scaffold(
+      appBar:AppBar(
+        title: const Text(App.name),), 
+        drawer: Drawer(
+          child:ListView(children: [
+          DrawerHeader(child: Text(_("navigation menu"))),
+          ListTile(title:Text(_("settings")) ,onTap:() async{
+            await Navigator.push(context, MaterialPageRoute(builder: (context) =>SettingsDialog(this._) ));
+            setState(() {
+              
+            });
+          } ,),
+          ListTile(title: Text(_("contect us")),onTap: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>ContectUsDialog(this._)));
+          },),
+          ListTile(title: Text(_("donate")),onTap: (){
+            launch("https://www.paypal.me/AMohammed231");
+          },),
+  ListTile(title: Text(_("visite project on github")),onTap: (){
+    launch("https://github.com/mesteranas/"+App.appName);
+  },),
+  ListTile(title: Text(_("license")),onTap: ()async{
+    String result;
+    try{
+    http.Response r=await http.get(Uri.parse("https://raw.githubusercontent.com/mesteranas/" + App.appName + "/main/LICENSE"));
+    if ((r.statusCode==200)) {
+      result=r.body;
+    }else{
+      result=_("error");
+    }
+    }catch(error){
+      result=_("error");
+    }
+    Navigator.push(context, MaterialPageRoute(builder: (context)=>ViewText(_("license"), result)));
+  },),
+  ListTile(title: Text(_("about")),onTap: (){
+    showDialog(context: context, builder: (BuildContext context){
+      return AlertDialog(title: Text(_("about")+" "+App.name),content:Center(child:Column(children: [
+        ListTile(title: Text(_("version: ") + App.version.toString())),
+        ListTile(title:Text(_("developer:")+" mesteranas")),
+        ListTile(title:Text(_("description:") + App.description))
+      ],) ,));
+    });
+  },)
+        ],) ,),
+        body:Container(alignment: Alignment.center
+        ,child: Column(children: [
+          if (!isRecording)
+          ElevatedButton(onPressed: () async{
+            await Permission.storage.request();
+            await Permission.microphone.request();
+            controller.start();
+            setState(() {
+              isRecording=true;
+            });
+          }, child: Text(_("record")))
+          else
+          ElevatedButton(onPressed: () async{
+            controller.stop();
+
+          }, child: Text(_("stop")))
+    ])),)));
+  }
+}
